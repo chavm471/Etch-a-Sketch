93ec1ce03ede01df1e0c31cd5c9a73f3a8024746
Marcos Chavez <mchav2@pdx.edu>
2024-06-25 10:48:10 -0700

    created the first square in my webpage

--- eas.js
+++ eas.js
@@ -1,1 +1,18 @@
 //this file is going to serve as the backend of my website
+
+//first insert the element
+const container = document.querySelector("#container");
+
+const y_axis = document.createElement("div");
+y_axis.classList.add("y_axis");
+
+//make into a box by editing attributes
+y_axis.style["background-color"] = "black";
+y_axis.style.borderStyle = "solid";
+y_axis.style.borderColor = "black";
+y_axis.style.height = "40px";
+y_axis.style.width = "40px";
+
+
+//add it to the DOM
+container.appendChild(y_axis);
