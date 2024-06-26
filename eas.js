b4373c2e6681d449875f267774911d7df6db71c9
Marcos Chavez <mchav2@pdx.edu>
2024-06-26 13:01:34 -0700

    I have the grid set up

--- eas.js
+++ eas.js
@@ -1,18 +1,20 @@
 //this file is going to serve as the backend of my website
+for (let i = 1; i < 256; ++i)
+{
+    //first insert the element
+    const container = document.querySelector("#container");
 
-//first insert the element
-const container = document.querySelector("#container");
+    const y_axis = document.createElement("div");
+    y_axis.classList.add("y_axis");
 
-const y_axis = document.createElement("div");
-y_axis.classList.add("y_axis");
+    //make into a box by editing attributes
+    y_axis.style["background-color"] = "black";
+    y_axis.style.borderStyle = "solid";
+    y_axis.style.borderColor = "black";
+    y_axis.style.width = "6.25%";
+    y_axis.style.height = "100px";
 
-//make into a box by editing attributes
-y_axis.style["background-color"] = "black";
-y_axis.style.borderStyle = "solid";
-y_axis.style.borderColor = "black";
-y_axis.style.height = "40px";
-y_axis.style.width = "40px";
+    //add it to the DOM
+    container.appendChild(y_axis);
+}
 
-
-//add it to the DOM
-container.appendChild(y_axis);
