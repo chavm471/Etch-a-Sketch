2de625531485eb6d6e9dd80d9904b30da2b83c54
Marcos Chavez <mchav2@pdx.edu>
2024-06-28 13:23:53 -0700

    I add rgb colors to random generate a color as you hover over a grid

--- eas.js
+++ eas.js
@@ -11,13 +11,18 @@ function clearGrid(){
 function getSize(){
     try{
         let gridSize = prompt("Enter the size. Has to be between 0 to 100.");
-
+        
+        //check if gridSize is empty
         if (gridSize === null){
             throw new Error("input cancelled by user");
         }
+        //check if gridsize is a number or less than 1 or greater than 100
         if (isNaN(gridSize) || gridSize < 1 || gridSize > 100){
+            //if true throw an error with a message
             throw new Error("Please enter a valid number.");
         }
+        //if no error thrown that means it meets criteria
+        //multipy to find the total num of boxes in grid
         let sum = gridSize * gridSize;
     
         //clear current grid
@@ -27,6 +32,7 @@ function getSize(){
     }
 
     catch{
+        //alert the user about the error
         alert (Error.message);
         //call recursive function again
         //meaning prompt the user again
@@ -34,7 +40,9 @@ function getSize(){
     }
 }
 
+//this function create the grid receiving the number of boxes it should have
 function createGrid(gridSize) {
+    //determine the width and height of each
     let width = (100/ Math.sqrt(gridSize)) + "vw";
     let height = (100/ Math.sqrt(gridSize)) + "vh";
     console.log(width);
@@ -67,7 +75,13 @@ function createGrid(gridSize) {
 
     temp.forEach((div) => {
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "white";
+            //div.style.backgroundColor = "white";
+            //random rgb colors
+            let num1 = Math.floor(Math.random() * 256);
+            let num2 = Math.floor(Math.random() * 256);
+            let num3 = Math.floor(Math.random() * 256);
+            
+            div.style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`;
         });
     });
 }
@@ -75,12 +89,13 @@ function createGrid(gridSize) {
 
 function main() {
     //reason for 256 is that 16 x 16 = 256 
+    //default size
     let size = 256;
 
     //call function to create initial grid
     createGrid(size);
 
-
+    //event listener for the button. calls the function getSize
     const btn = document.querySelector(".prompt");
 
     btn.addEventListener("click", getSize);
